b324940090bb5cd1363c42b968c6d192fe33cca7
IrfanMunir1 <irfan.munir@algo.com>
2022-02-21 15:49:24 +0500

    Do refactoring

--- server.js
+++ server.js
@@ -16,7 +16,52 @@ app.use(bodyParser.json());
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
+app.post('/users',(req,res)=>{
+  User.create(
+    {...req.body.newData},
+    (err,data)=>{sendResponse(res,err,data)}
+  )
+})
 
+app.route('/users/:id')
+.get((req,res)=>{
+  User.findById(
+    req.params.id,
+    (err,data)=>{sendResponse(res,err,data)})
+})
+.put((req,res)=>{
+  User.findByIdAndUpdate(
+    req.params.id,
+    {...req.body.newData},
+    {new:true},
+    (err,data)=>{sendResponse(res,err,data)})
+})
+.delete((req,res)=>{
+  User.findByIdAndDelete(
+    req.params.id,
+    (err,data)=>{sendResponse(res,err,data)})
+})
+
+function sendResponse(res,err,data){
+  if (err){
+    res.json({
+      success: false,
+      message: err
+    })
+  } else if (!data){
+    res.json({
+      success: false,
+      message: "Not Found"
+    })
+  } else {
+    res.json({
+      success: true,
+      data: data
+    })
+  }
+}
+
+/* old plain way , Its refactor as above
 // CREATE
 app.post('/users',(req,res)=>{
   User.create(
@@ -113,4 +158,4 @@ app.route('/users/:id')
       }
     }
   )
-})
+})*/
